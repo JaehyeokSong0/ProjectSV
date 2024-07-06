86e30fc359340f885748a24e457fed77650e1b57
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2024-07-06 19:05:48 +0900

    Implement Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Manages game time
+/// </summary>
+public class Timer : MonoBehaviour
+{
+    // TEST CODE
+    private void Start()
+    {
+        StartCheckTime();
+    }
+    #region Property
+    public float ElapsedTime => _elapsedTime;
+    #endregion
+
+    #region Field
+    private float _elapsedTime = 0f;
+    private Coroutine _checkTimeCoroutine = null;
+    #endregion
+
+    #region Method
+    public void ResetCheckTime()
+    {
+        _elapsedTime = 0f;
+    }
+
+    public void StartCheckTime()
+    {
+        _checkTimeCoroutine = StartCoroutine(C_StartCheckTime());
+    }
+    private IEnumerator C_StartCheckTime()
+    {
+        while (true)
+        {
+            _elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void StopCheckTime()
+    {
+        if (_checkTimeCoroutine != null)
+            StopCoroutine(_checkTimeCoroutine);
+        else
+            Debug.LogError("Cannot stop checkTimeCoroutine");
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,25 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    #region Field
+    [SerializeField] private Timer _timer;
+    [SerializeField] private TMP_Text _timeText;
+    #endregion
+
+    #region Event Method
+    private void Awake()
+    {
+        if ( _timer == null )
+            _timer = GameObject.FindFirstObjectByType<Timer>();
+        if(_timeText == null )
+            _timeText = transform.GetChild(0).GetComponent<TMP_Text>();
+    }
+    private void LateUpdate()
+    {
+        _timeText.text = $"{TimeSpan.FromSeconds(_timer.ElapsedTime).ToString(@"mm\:ss")}";
+    }
+    #endregion
+}
